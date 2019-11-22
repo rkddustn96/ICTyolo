5871c2aec8be6f0f1f7c553c1c959c09532af75d
rkddustn96 <rkddustn96@naver.com>
2019-11-22 18:17:55 +0900

    change movie file-> wab cam input

--- movingrecognition/moving object.py
+++ movingrecognition/moving object.py
@@ -1,9 +1,15 @@
 #영상출저 : https://www.youtube.com/watch?v=FuCVRbUZZ2s
 #코드출저 : https://webnautes.tistory.com/1248
+
 import cv2
 import numpy as np
 
-cap = cv2.VideoCapture("moving test.mp4")
+cap = cv2.VideoCapture(0)
+cap.set(3, 640) #WIDTH
+cap.set(4, 480) #HEIGHT
+
+#cap = cv2.VideoCapture("moving test.mp4")
+
 fgbg = cv2.createBackgroundSubtractorMOG2(varThreshold=100)
 
 while(1):
@@ -20,7 +26,7 @@ while(1):
         x, y, width, height, area = stats[index]
         centerX, centerY = int(centroid[0]), int(centroid[1])
 
-        if area > 100:
+        if area > 150:
             cv2.circle(frame, (centerX, centerY), 1, (0, 255, 0), 2)
             cv2.rectangle(frame, (x, y), (x + width, y + height), (0, 0, 255))
 
